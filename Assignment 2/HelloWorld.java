d02a45ba54dacd27d05a52b4b06719f3e51efc87
ReyRiordan <59772807+ReyRiordan@users.noreply.github.com>
2023-01-08 21:09:20 -0500

    Create HelloWorld.java

---
+++ Assignment 2/HelloWorld.java
@@ -0,0 +1,21 @@
+/*************************************************************************
+ *  Compilation:  javac HelloWorld.java
+ *  Execution:    java HelloWorld
+ *
+ *  @author: Rey Riordan reyriordan@gmail.com rhr58
+ *
+ *  Prints "Hello, World". By tradition, this is everyone's first program.
+ *
+ *  % java HelloWorld
+ *  Hello, World
+ *
+ *************************************************************************/
+
+public class HelloWorld {
+
+  public static void main(String[] args) {
+        
+	System.out.println("Hello, World");
+  }
+
+}
